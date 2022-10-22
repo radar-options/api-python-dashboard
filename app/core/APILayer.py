7bf17b433b9b6fa0bcf482f2a6b5609ad95af023
Ernesto Martínez del Pino <ernestomar1997@hotmail.com>
2022-10-23 00:49:46 +0200

    cd run DO publish

--- app/core/APILayer.py
+++ app/core/APILayer.py
@@ -6,6 +6,9 @@ class APILayer:
     def __init__(
         self,
         mongodb_uri: str,
+        cboe_collection_name: str = None,
+        spot_collection_name: str = None,
+        open_interest_collection_name: str = None
     ):
         self.query_manager = QueryManager(mongodb_uri)
 
